c8b2daab3541ace1c0a9e1e8ed9bbbe31c58bec9
imranj131 <imranj131@yahoo.com>
2015-08-03 12:02:00 -0400

    updated with company code test

--- test/unit/ble/data/gap/manufacturerspecificdata.test.js
+++ test/unit/ble/data/gap/manufacturerspecificdata.test.js
@@ -12,13 +12,14 @@ var CURSOR = 0;
 var ADVERTISER_DATA = {};
   
 // Inputs for the scenario
-var INPUT_DATA_COMPANY_ONLY = '03ff8c00';
+var INPUT_DATA_COMPANY_ONLY = '03ff0800';
 var INPUT_DATA_IBEACON = 
                       '26ff4c000215b9407f30f5f8466eaff925556b57fe6d294c903974';
 
 // Expected outputs for the scenario
 var EXPECTED_DATA_COMPANY_ONLY = {
-  companyIdentifierCode: "008c",
+  companyName: "Motorola",
+  companyIdentifierCode: "0008",
   data: ""
 };
 var EXPECTED_DATA_APPLE_AND_IBEACON = {
@@ -31,7 +32,7 @@ var EXPECTED_DATA_APPLE_AND_IBEACON = {
 describe('ble data manufacturerspecificdata', function() {
 
   // Test the process function
-  it('should convert ble advertiser data to a apple manufacturer specific \
+  it('should convert ble advertiser data to a company manufacturer specific \
      data', function() {
     manufacturerspecificdata.process(INPUT_DATA_COMPANY_ONLY, CURSOR, 
                                      ADVERTISER_DATA);
