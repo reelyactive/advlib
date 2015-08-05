0a0416146057dfa71921518e3acc2f882a0f5192
imranj131 <imranj131@yahoo.com>
2015-08-04 22:02:07 -0400

    Updated EXPECTED_DATA_COMPANY_NAME with uriBeacon & fixed mistake in i/o for test 2. TO DO: companyName error

--- test/unit/ble/data/gap/servicedata.test.js
+++ test/unit/ble/data/gap/servicedata.test.js
@@ -1,4 +1,3 @@
-
 /**
  * Copyright reelyActive 2015
  * We believe in an open Internet of Things
@@ -7,34 +6,44 @@
 var servicedata = require("../../../../../lib/ble/data/gap/servicedata.js");
 var assert = require('assert'); 
 
+
 // Constants for the scenario
 var CURSOR = 0;
 var ADVERTISER_DATA = {};
 
 // Inputs for the scenario
-var INPUT_DATA = '09160a181204eb150000';
+var INPUT_DATA = '09164c001204eb150000';
 var INPUT_DATA_COMPANY_NAME = '1216d8fe00f2027265656c7961637469766507';
 
 // Expected outputs for the scenario
 var EXPECTED_DATA = {
-  uuid: "180a",
-  data: "1204eb150000",
+  serviceData: {
+    uuid: "004c",
+    data: "1204eb150000",
+  }
 };
 var EXPECTED_DATA_COMPANY_NAME = {
-  uuid: "fed8",
-  data: "00f2027265656c7961637469766507",
-  companyName: "Google"
-}
+  serviceData: {
+    uuid: "fed8",
+    data: "00f2027265656c7961637469766507",
+    companyName: "Google",
+    uriBeacon: {
+      invisibleHint: false,
+      txPower: "-14dBm",
+      url: "http://reelyactive.com"
+    }
+  }
+};
 
 describe('ble data servicedata', function() {
 
   // Test the process function
   it('should parse BLE advertiser data service data', function() {
     servicedata.process(INPUT_DATA, CURSOR, ADVERTISER_DATA);
-    assert.deepEqual(ADVERTISER_DATA.serviceData, EXPECTED_DATA);
+    assert.deepEqual(ADVERTISER_DATA, EXPECTED_DATA);
   });
   it('should parse BLE advertiser data service data with companyName', function() {
-    servicedata.process(INPUT_DATA, CURSOR, ADVERTISER_DATA);
-    assert.deepEqual(ADVERTISER_DATA.serviceData, EXPECTED_DATA);
+    servicedata.process(INPUT_DATA_COMPANY_NAME, CURSOR, ADVERTISER_DATA);
+    assert.deepEqual(ADVERTISER_DATA, EXPECTED_DATA_COMPANY_NAME);
   });
 });
