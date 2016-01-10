4f58612790ddee6ea67778834ae253a1a555aa61
jeffyactive <jeff@reelyactive.com>
2016-01-10 17:59:32 -0500

    Updated BLE presets

--- web/js/advapp.js
+++ web/js/advapp.js
@@ -14,38 +14,53 @@ module.exports = angular.module('advapp', ['ui.bootstrap'])
     $scope.bluetooth = {}; // TODO: remove once removed from index.html
     $scope.packet = {};
     $scope.presets = [{
-      name: "reelyActible",
-      payload: "421655daba50e1fe0201050c097265656c79416374697665"
+      name: "reelyActive Bluetooth Smart reelceiver (RA-R436)",
+      payload: "061b9e5ed0f7b13402010611074449555520657669746341796c656572"
     }, {
-      name: "Apple TV",
-      payload: "0015415df17b209c02011a0bff4c0009060202c0a8006a"
+      name: "reelyActive iOS SDK (Foreground)",
+      payload: "401fb67d3eefc36b02011a1107ffeeddccbbaa9988776655443322110003092d2d"
     }, {
-      name: "Roximity",
-      payload: "42243cae3eb8ebe00201061aff4c0002158deefbb9f7384297804096668bb4428100012258c5"
+      name: "reelyActive iOS SDK (Background)",
+      payload: "401eb67d3eefc36b02011a14ff4c000100000000000040000000000000000000"
     }, {
-      name: "UriBeacon",
-      payload: "4220f60032be32c20201040303d8fe1216d8fe00f2027265656c7961637469766507"
+      name: "Scanning device",
+      payload: "430cb67d3eefc36ba28c2d8cba1c"
     }, {
-      name: "Tile",
-      payload: "400dc3dc80ee20e40201060303edfe"
+      name: "Eddystone (URL)",
+      payload: "421953ce2b1cec4e1216aafe109f027265656c7961637469766507"
     }, {
-      name: "Bright Beacon",
-      payload: "40241582727dbefb0201061aff4c000215e2c56db5dffb48d2b060d0f5a71096e0fbbe7d72bf0040"
+      name: "Fitbit",
+      payload: "40250000004f01d90201041106ba5689a6fabfa2bd01467d6e00fbabad09160a181204eb150000"
     }, {
       name: "Xiaomi Mi Band",
       payload: "0013dd748d100f880201060502e0fee7fe03094d490041"
     }, {
-      name: "Estimote",
+      name: "Tile",
+      payload: "400dc3dc80ee20e40201060303edfe"
+    }, {
+      name: "Flic",
+      payload: "00253ac870dae48002010611071bc5d5a50200dc9ee411e726c0df2af0090966303132634d6736"
+    }, {
+      name: "Apple (Home products)",
+      payload: "00150000007b209c02011a0bff4c0009060202c0a8006a"
+    }, {
+      name: "Nexus Player",
+      payload: "001b0000005979f002011a1107fc9dd0b3cb84e0840642f3f7e1e0bfcb"
+    },  {
+      name: "Roximity (iBeacon)",
+      payload: "42243cae3eb8ebe00201061aff4c0002158deefbb9f7384297804096668bb4428100012258c5"
+    }, {
+      name: "Bright Beacon (iBeacon)",
+      payload: "40241582727dbefb0201061aff4c000215e2c56db5dffb48d2b060d0f5a71096e0fbbe7d72bf0040"
+    }, {
+      name: "Estimote (iBeacon)",
       payload: "4024a4b9297143fb0201061aff4c000215b9407f30f5f8466eaff925556b57fe6d03510003b60035"
     }, {
-      name: "Fitbit",
-      payload: "4025b2a86b4f01d90201041106ba5689a6fabfa2bd01467d6e00fbabad09160a181204eb150000"
-    }, {
-      name: "Kontakt.io",
+      name: "Kontakt.io (iBeacon)",
       payload: "402494eb4ce223ea0201061aff4c000215f7826da64fa24e988024bc5b71e0893ef7e84be5b30036"
     }, {
-      name: "reelyActive Bluetooth Smart reelceiver (RA-R436)",
-      payload: "061b9e5ed0f7b13402010611074449555520657669746341796c656572"
+      name: "UriBeacon",
+      payload: "4220f60032be32c20201040303d8fe1216d8fe00f2027265656c7961637469766507"
     }];
 
     $scope.headerType = [
