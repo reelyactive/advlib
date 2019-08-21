ed9f1fe0a1f28cc171845db6d04f89e977a3c99c
jeffyactive <jeff@reelyactive.com>
2019-08-21 14:21:01 -0400

    Accept that uppercase addresses are converted to lowercase

--- test/unit/ble/address/index.test.js
+++ test/unit/ble/address/index.test.js
@@ -29,22 +29,22 @@ describe('ble address', function() {
 
   it('should not convert a short hexadecimal address to JSON', function() {
 	    assert.notDeepEqual(advlib.process(INPUT_DATA_TOO_SHORT), 
-                                         EXPECTED_DATA_VALID);
+                                EXPECTED_DATA_VALID);
   });
 
   it('should convert a long hexadecimal address to JSON', function() {
 	    assert.deepEqual(advlib.process(INPUT_DATA_TOO_LONG), 
-                                      EXPECTED_DATA_VALID);
+                             EXPECTED_DATA_VALID);
   });
 
   it('should not convert a non hexadecimal address to JSON', function() {
 	    assert.notDeepEqual(advlib.process(INPUT_DATA_NOT_HEX), 
-                                         EXPECTED_DATA_VALID);
+                                EXPECTED_DATA_VALID);
   });
   
-  it('should not convert a hexadecimal address with upper case letters to JSON',
+  it('should convert a hexadecimal address with upper case letters to JSON',
      function() {
-	    assert.notDeepEqual(advlib.process(INPUT_DATA_UPPER_CASE_LETTERS), 
-                                         EXPECTED_DATA_VALID);
+	    assert.deepEqual(advlib.process(INPUT_DATA_UPPER_CASE_LETTERS), 
+                             EXPECTED_DATA_VALID);
   });
 });
