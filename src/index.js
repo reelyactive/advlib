96800cf85e775421548144c1a0c654a086887850
jeffyactive <jeff@reelyactive.com>
2024-02-12 19:50:29 -0500

    Updated to latest packages

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ export const ESP_PROCESSOR = {
     processor: require('advlib-esp'),
     libraries: [ require('advlib-eep-vld'),
                  require('advlib-eep-4bs'),
-                 require('advlib-eep-rps') ],
+                 require('advlib-eep-rps'),
+                 require('advlib-eep-msc') ],
     options: { ignoreProtocolOverhead: false }
 };
 export const INTERPRETERS = [ require('advlib-interoperable') ];
